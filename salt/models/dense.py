619d9eaa0becec1fd9bca66c32e13aa01d51a292
Matthew Leigh <matthew.leigh@cern.ch>
2023-01-31 16:27:18 +0100

    New attention and transformer layers (updated)

--- salt/models/dense.py
+++ salt/models/dense.py
@@ -1,4 +1,52 @@
-from torch import Tensor, nn
+from typing import Optional
+
+from torch import Tensor, concat, nn
+
+
+def attach_context(x: Tensor, context: Tensor) -> Tensor:
+    """Concatenates a context tensor to an input tensor with considerations for
+    broadcasting.
+
+    The idea behind this is to allow a context tensor less or equal dimensions to be
+    concatenated to an input with more dimensions.
+    This function checks the dimension difference and reshapes the context to have
+    the same dimension as the constituents so broadcast concatenation can apply.
+    The shape change always assumes that the first dimension is the batch and the last
+    are the features.
+
+    Here is a basic use case: concatenating the high level jet variables to the
+    constituents during a forward pass through the network
+    - The jet variables will be of shape: [batch, j_features]
+    - The constituents will be of shape: [batch, num_nodes, n_features]
+    Here the jet variable will be shaped to [batch, 1, j_features] allowing broadcast
+    concatenation with the constituents
+
+    Another example is using the edge features [b,n,n,ef] and concatenating the
+    high level jet variables, which will be expanded to [b,1,1,jf] or the conditioning
+    on the node features which will be expanded to [b,1,n,nf]
+    """
+    if context is None:
+        raise RuntimeError("Expected context is missing from forward pass")
+
+    # Check if the context information has less dimensions and the broadcast is needed
+    dim_diff = x.dim() - context.dim()
+
+    # Context cant have more dimensions
+    if dim_diff < 0:
+        raise ValueError(
+            f"Provided context has more dimensions ({context.dim()}) than inputs ({x.dim()})"
+        )
+
+    # If reshaping is required
+    if dim_diff > 0:
+        # Reshape the context inputs with 1's after the batch dim
+        context = context.view(context.shape[0], *dim_diff * (1,), *context.shape[1:])
+
+        # Use expand to allow for broadcasting as expand does not allocate memory
+        context = context.expand(*x.shape[:-1], -1)
+
+    # Apply the concatenation on the final dimension
+    x = concat([x, context], dim=-1)
 
 
 class Dense(nn.Module):
@@ -12,8 +60,10 @@ class Dense(nn.Module):
         norm_layer: str = None,
         norm_final_layer: bool = False,
         dropout: float = 0.0,
-    ):
-        """A simple fully connected feed forward neural network)
+        context_size: int = 0,
+    ) -> None:
+        """A simple fully connected feed forward neural network, which can take
+        in additional contextual information.
 
         Parameters
         ----------
@@ -33,11 +83,18 @@ class Dense(nn.Module):
             Whether to use normalisation on the final layer, by default False
         dropout : float, optional
             Apply dropout with the supplied probability, by default 0.0
+        context_size : int
+            Size of the context tensor, 0 means no context information is provided
         """
         super().__init__()
 
+        # Save the networks input and output sizes
+        self.input_size = input_size
+        self.output_size = output_size
+        self.context_size = context_size
+
         # build nodelist
-        node_list = [input_size, *hidden_layers, output_size]
+        node_list = [input_size + context_size, *hidden_layers, output_size]
 
         # input and hidden layers
         layers = []
@@ -68,5 +125,7 @@ class Dense(nn.Module):
         # build the net
         self.net = nn.Sequential(*layers)
 
-    def forward(self, x: Tensor):
+    def forward(self, x: Tensor, context: Optional[Tensor] = None) -> Tensor:
+        if self.context_size:
+            x = attach_context(x, context)
         return self.net(x)
