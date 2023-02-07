a3f76d10a48b65fe81906304c237d6ee7686b42b
Samuel Van Stroud <sam.van.stroud@cern.ch>
2023-02-07 11:09:48 +0100

    Base config relative to main.py

--- salt/main.py
+++ salt/main.py
@@ -1,5 +1,7 @@
 """Top level training script, powered by the lightning CLI."""
 
+import pathlib
+
 import comet_ml  # noqa F401
 from pytorch_lightning.cli import ArgsType
 
@@ -7,6 +9,8 @@ from salt.callbacks import SaveConfigCallback
 from salt.data.datamodules import JetDataModule
 from salt.lightning import LightningTagger
 from salt.utils.cli import SaltCLI
+
+config_dir = pathlib.Path(__file__).parent / "configs"
 
 
 def main(args: ArgsType = None) -> None:
@@ -17,8 +21,8 @@ def main(args: ArgsType = None) -> None:
         args=args,
         parser_kwargs={
             "default_env": True,
-            "fit": {"default_config_files": ["configs/base.yaml"]},
-            "test": {"default_config_files": ["configs/base.yaml"]},
+            "fit": {"default_config_files": [f"{config_dir}/base.yaml"]},
+            "test": {"default_config_files": [f"{config_dir}/base.yaml"]},
         },
     )
 
